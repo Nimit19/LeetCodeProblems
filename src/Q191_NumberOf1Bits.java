ef5ca53bdffdff255f0a7830b24e76ef7498b5db
Nimit Patel <nimitpatelnp19@gmail.com>
2022-07-10 23:44:45 +0530

    Initial Commit

---
+++ src/Q191_NumberOf1Bits.java
@@ -0,0 +1,16 @@
+public class Q191_NumberOf1Bits {
+    public static void main(String[] args) {
+        int n = 521;
+        System.out.println(hammingWeight(n));
+    }
+    static int hammingWeight(int n) {
+
+            int count = 0;
+            for(int i=1; i<33; i++){
+                if((n & (1 << i)) != 0){
+                    count++;
+                }
+            }
+            return count;
+    }
+}
