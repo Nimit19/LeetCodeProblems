adee4ca44f516d90e2a67c90874cd3f02092f3bf
Nimit Patel <nimitpatelnp19@gmail.com>
2022-07-10 22:37:12 +0530

    Initial Commit

--- src/Q1281_SubtractTheProductAndSum.java
+++ src/Q1281_SubtractTheProductAndSum.java
@@ -1,5 +1,17 @@
 public class Q1281_SubtractTheProductAndSum {
     public static void main(String[] args) {
+        int n = 4421;
+        System.out.println(subtractProductAndSum(n));
+    }
 
+    static int subtractProductAndSum(int n) {
+        int r,sum = 0,product = 1;
+        while(n > 0){
+            r = n % 10;
+            sum = sum + r;
+            product = product * r;
+            n = n /10;
+        }
+        return (product - sum);
     }
 }
